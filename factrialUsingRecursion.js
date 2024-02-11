f873918661a882f67236097051f14ecffdab6c41
AnandIsCoding <159451016+AnandIsCoding@users.noreply.github.com>
2024-02-11 23:25:32 +0530

    factrialUsingRecursion.js

--- factrialUsingRecursion.js
+++ factrialUsingRecursion.js
@@ -1,9 +1,14 @@
 function factorial(n){
-	if(n===0 || n===1){
-		return 1;
+	if(!isNaN(n) && !(n<0)){
+		if(n===0 || n===1){
+			return 1;
+		}else{
+			return n*factorial(n-1);
+		}
 	}else{
-		return n*factorial(n-1);
+		return `enter a non negative number`
 	}
+	
 }
 console.log(factorial(5)) 		//  120
 console.log(factorial(3))		//  6
