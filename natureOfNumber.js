ae424c4c15339544d6b48c22fac438b537659f6b
AnandIsCoding <159451016+AnandIsCoding@users.noreply.github.com>
2024-02-11 12:17:31 +0530

    Add files via upload

---
+++ natureOfNumber.js
@@ -0,0 +1,6 @@
+/*
+Use a nested ternary operator to check that a number is positive, negative or zero. You have to print “positive” if the number is positive and similarly for negative and zero also
+*/
+
+let num=0;
+let result= (num>0)? console.log(`positive`):(num<0)? console.log(`negative`):console.log(`zero`);
