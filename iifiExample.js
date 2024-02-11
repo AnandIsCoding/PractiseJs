6fb5d847e76be09db613997d06d039035d45de1a
AnandIsCoding <159451016+AnandIsCoding@users.noreply.github.com>
2024-02-11 22:51:55 +0530

    iifiExample.js

---
+++ iifiExample.js
@@ -0,0 +1,16 @@
+
+(
+	function sqr(n){
+		console.log(n*n)
+	}
+)(5);
+
+                         // output :  25
+
+(
+	function sqr(n){
+		console.log(5*5)
+	}
+)();
+
+                        // output :  25
